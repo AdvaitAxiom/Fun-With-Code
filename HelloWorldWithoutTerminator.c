a2dca95de12c304b637950c69ee350682b41a2f9
Alapan000 <alapandas2810@gmail.com>
2025-04-13 11:49:15 +0530

    Hello World without using ;

---
+++ HelloWorldWithoutTerminator.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    if(printf("Hello World!")){}
+
+    //Here printf is in the control statement so it have to return
+    //printf returns 12 after printing it(No of characters)
+    //if get 12(!=0)( = true) so if body executes
+}
