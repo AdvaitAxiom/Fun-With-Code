cb79f9d44168c0af7710a045117175d500a992c2
Alapan000 <alapandas2810@gmail.com>
2025-04-13 15:40:17 +0530

    Compare values in java

---
+++ Compare.java
@@ -0,0 +1,11 @@
+public class Compare {
+    public static void main(String[] args) {
+        int a = 128;
+        int b = 128;
+
+        Integer c = 128;
+        Integer d = 128;
+        System.out.println(a==b);
+        System.out.println(c==d);
+    }
+}
